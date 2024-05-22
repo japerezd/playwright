c3d9b1d15fe5545aca26020b608a141a1c7bf7bb
Jorge Perez <jorge_alberto1997@hotmail.com>
2024-05-21 22:28:59 -0500

    Add locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,32 +2,27 @@ import { test } from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:4200/');
+  await page.getByText('Forms').click();
+  await page.getByText('Form Layouts').click();
 });
 
-test.describe('suite1', () => {
-  test.beforeEach(async ({page}) => {
-    await page.getByText('Charts').click();
-  });
-  
-  test('the first test', async ({page}) => {
-    
-  });
-  
-  test('navigate to datepicker page', async ({page}) => {
-    await page.getByText('Datepicker').click();
-  });
-});
-
-test.describe('suite2', () => {
-  test.beforeEach(async ({page}) => {
-    await page.getByText('Forms').click();
-  });
-  
-  test('the first test2', async ({page}) => {
-    await page.getByText('Form Layouts').click();
-  });
-  
-  test('navigate to datepicker page2', async ({page}) => {
-    await page.getByText('Datepicker').click();
-  });
+test('Locator syntax rules', async ({page}) => {
+  // by tag name
+  page.locator('input')
+  //by id
+  await page.locator('#inputEmail1').click();
+  // by class val
+  page.locator('.shape-rectangle')
+  // by attribute
+  page.locator('[placeholder="Email"]')
+  // by class value (full)
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+  // combine different selectors
+  page.locator('input[placeholder="Email"][nbinput]')
+  // by XPath (not recommended)
+  page.locator('//*[@id="inputEmail1"]')
+  // by partial text match
+  page.locator(':text("Using")')
+  // by exact text match
+  page.locator(':text-is("Using the Grid")');
 });
